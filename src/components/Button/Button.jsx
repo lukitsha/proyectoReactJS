f32a7a535380604fb40e428037a4e57b74f3f169
lukitsha <lucagaido_@hotmail.com>
2024-03-03 00:37:33 -0300

    se agrego utilidad a los botones de la lista de categorias (por ahora solo console.log

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,6 @@
 const Button = (props) => {
     console.log(props)
-    return <button style={{background: props.bgColor, color: props.fontColor}} className={props.className}>{props.label}</button>
+    return <button onClick={props.callback} style={{background: props.bgColor, color: props.fontColor}} className={props.className}>{props.label}</button>
 }
 
 export default Button
