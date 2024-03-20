154e7ec7588bcd08e5106211ac345551d2c9f90d
lukitsha <lucagaido_@hotmail.com>
2024-03-20 19:56:25 -0300

    Se corrigieron errores menores

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,29 @@ import Navbar from "./components/Navbar/Navbar"
 import ListadoCategorias from "./components/ListadoCategorias/ListadoCategorias"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailConteiner"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
-  const [page, setPage] = useState('list')
+  //const [page, setPage] = useState('list')
     
 
   return (
     <div className="App">
 
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
       
-      <h1>Lekets Electronics</h1>
-      <h3>Electronic Wharehouse</h3>
-      
-      { page === 'list' && <ItemListContainer greeting='Bienvenidos al Almacen Vitrual' /> }
-      { page === 'detail' && <ItemDetailContainer /> }
-      
-      <ListadoCategorias />
-      
+        <h1>Lekets Electronics</h1>
+        <h3>Electronic Wharehouse</h3>
+
+        <Routes>
+          <Route path='/' element={<ItemListContainer greeting='Bienvenidos al Almacen Vitrual' /> }></Route>
+          <Route path='/item' element={<ItemDetailContainer /> }></Route>
+          
+        </Routes>
+
+        <ListadoCategorias />
+      </BrowserRouter>
       
       
       
