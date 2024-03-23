3fc8b73ee841ece36e88cda48e2385a1a97de291
lukitsha <lucagaido_@hotmail.com>
2024-03-23 14:50:46 -0300

    se importaron mas estilos

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,23 +1,23 @@
-import './Item.module.css'
+import styles from './Item.module.css'; 
 import { Link } from "react-router-dom"
 
 const Item = ({id, name, img, price, stock}) => {
 
     return(
-        <article className="CardItem">
-            <header className="Header">
-                <h2 className="ItemHeader">
+        <article className={styles.CardItem}> 
+            <header className={styles.Header}> 
+                <h2 className={styles.ItemHeader}> 
                     {name}
                 </h2>
             </header>
             <picture>
-                <img src={img} style={{ width: 175 }} />
+                <img src={img} style={{ width: 175}} />
             </picture>
             <section>
-                <p className="Info">
+                <p className={styles.Info}> 
                     Precio: U$D{price}
                 </p>
-                <p className="Info">
+                <p className={styles.Info}>
                     Cantidad en Stock: {stock}
                 </p>
             </section>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,34 +1,31 @@
-import { useEffect, useState } from "react"
-import { getProducts, getProductsByCategory } from "../../asyncMock"
-import ItemList from "../ItemList/ItemList"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { getProducts, getProductsByCategory } from "../../asyncMock";
+import ItemList from "../ItemList/ItemList";
+import { useParams } from "react-router-dom";
+import styles from "./ItemListContainer.module.css";
 
 const ItemListContainer = ({ greeting }) => {
+  const [products, setProducts] = useState([]);
+  const { categoryId } = useParams();
 
-    const [products, setProducts] = useState([])
+  useEffect(() => {
+    const asyncFunc = categoryId ? getProductsByCategory : getProducts;
 
-    const { categoryId } = useParams ()
+    asyncFunc(categoryId)
+      .then((response) => {
+        setProducts(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [categoryId]);
 
-    useEffect(() => {
-        const asyncFunc = categoryId ? getProductsByCategory : getProducts
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{greeting}</h1>
+      <ItemList products={products} />
+    </div>
+  );
+};
 
-        asyncFunc(categoryId)
-            .then(response => {
-                setProducts(response)
-            })
-            .catch(error => {
-                console.error(error)
-            })
-    }, [categoryId])
-
-    console.log(products)
-
-    return (
-        <div>
-            <h1>{ greeting }</h1>
-            <ItemList products={products} />
-        </div>
-    )
-}
-
-export default ItemListContainer
+export default ItemListContainer;
