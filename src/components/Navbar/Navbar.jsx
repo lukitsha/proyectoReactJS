4980070fdea7c7a105bd5cc87d650bc410bc4e9a
lukitsha <lucagaido_@hotmail.com>
2024-04-23 19:21:06 -0300

    cambios menores

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,20 +22,28 @@ const Navbar = () => {
                 })
                 setCategories(categoriesAdapted)
             })
-            
-    }) 
+            .catch(error => {
+                console.error('error')
+            })
+    }, []) 
 
     return (
         <header className={classes.header}>
-            <h2 onClick={() => navigate('/')} className='tituloNav'>LeketsElectronics</h2>
+            <h2 onClick={() => navigate('/')} style={{cursor: 'pointer'}}>LeketsElectronics</h2>
             <nav className='NavBar'>
+                {
+                    categories.map(cat => {
+                        return <Link key={cat.id} to={`/category/${cat.slug}`}>{cat.name}lala</Link>
+                    })
+                }
                 <button className='btn btn-secondary'><Link to='/category/placasDeVideo'>Placas de Video</Link></button>
                 <button className='btn btn-secondary'><Link to='/category/vapes'>Vapes</Link></button>
                 <button className='btn btn-secondary'><Link to='/category/otros'>Otros</Link> </button>
-            </nav>
+                </nav>
             <CartWidget />
         </header>
     )
 }
 
 export default Navbar
+
